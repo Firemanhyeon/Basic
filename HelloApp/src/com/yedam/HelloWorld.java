33c4170d6effcd2530574c87356f113066d26f26
admin <admin@DESKTOP-9QDJ81V>
2023-05-30 10:46:30 +0900

    Another_World

--- HelloApp/src/com/yedam/HelloWorld.java
+++ HelloApp/src/com/yedam/HelloWorld.java
@@ -1,11 +1,14 @@
-package com.yedam;
-import java.util.*;
+	package com.yedam;
+	import java.util.*;
+
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello, World!!!!!");
-		System.out.println("git 연동.");
-		
-	}
 
+		public static void main(String[] args) {
+			System.out.println("Hello, World!!!!!");
+			System.out.println("git 연동.");
+			
+		
+
+	}
 }
